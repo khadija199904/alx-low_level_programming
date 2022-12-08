1e2f07ee04c43152e8f999748f666d4e16b8c938
khadija199904 <khadijaelabbioui1999@gmail.com>
2022-12-08 12:26:05 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,23 @@
-#include<stdio.h>
-int main (void)
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return (O);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
